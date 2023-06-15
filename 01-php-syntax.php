5ff49fc2777a5a228856de4d13e38de75e318dd8
Aaron Saya <60886005+AaronSaya@users.noreply.github.com>
2023-06-15 10:52:58 +0200

    php-syntax #1

---
+++ 01-php-syntax.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+
+<h1> My first PHP -page</h1>
+
+//Basic PHP syntax//
+<?php 
+echo "Hello World!<br>";
+?>
+<br>
+
+
+//PHP Case Sensitivity//
+<br><br>
+<?php
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World<br>";
+?>
+<br>
+
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?>
+
+
+
+</body>
+
+
+
+</html>
